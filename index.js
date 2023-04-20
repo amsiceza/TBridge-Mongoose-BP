8842f931ed39c50980835ad77c0829f742be3d21
GuileSoler87 <guillermosoler87@gmail.com>
2023-04-20 17:41:05 +0200

    index.js updated with routes

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const PORT = 8080;
 const { dbConnection } = require("./config/config")
 
 app.use(express.json())
-
+app.use('/users', require('./routes/users'));
+app.use('/posts', require('./routes/posts'));
+app.use('/comments', require('./routes/comments'));
 
 dbConnection()
 
