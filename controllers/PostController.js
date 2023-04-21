8ac163dc3cdda019b3c5cc67e3661af4a391577d
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-04-21 13:22:02 +0200

    Update create Post for UserId

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -3,7 +3,10 @@ const Post = require("../models/post");
 const PostController = {
     async create(req, res) {
         try {
-            const newPost = await Post.create(req.body)
+            const newPost = await Post.create({
+                ...req.body,
+                userId: req.user._id,
+            })
             res.status(201).send(newPost)
         } catch (error) {
             console.error(error)
