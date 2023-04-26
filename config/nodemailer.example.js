0c36d9e815d5ac0163b06cbb05f02d889ef48d87
GuileSoler87 <guillermosoler87@gmail.com>
2023-04-26 17:45:16 +0200

    nodemailer.example added

---
+++ config/nodemailer.example.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+host: 'smtp.gmail.com',
+port: 465,
+secure: true,
+auth: {
+user: 'your-mail@gmail.com',
+pass: null
+}
+});
+module.exports = transporter;
