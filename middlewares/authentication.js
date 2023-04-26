79c9bf079ef5dbb3ef54386e732708402190c574
GuileSoler87 <guillermosoler87@gmail.com>
2023-04-26 17:51:40 +0200

    Translations of middleware responses

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -10,13 +10,13 @@ const authentication = async (req, res, next) => {
         const payload = jwt.verify(token, jwt_secret);
         const user = await User.findOne({ _id: payload._id, tokens: token });
         if (!user) {
-            return res.status(401).send({ message: 'No estas autorizado' });
+            return res.status(401).send({ message: 'You are not authorized' });
         }
         req.user = user;
         next();
     } catch (error) {
         console.error(error)
-        return res.status(500).send({ error, message: 'Ha habido un problema con el token' })
+        return res.status(500).send({ error, message: 'There was a problem with the tokenn' })
     }
 }
 
@@ -34,15 +34,15 @@ const isAuthor = async (req, res, next) => {
     try {
         const post = await Post.findById(req.params._id);
         if (!post) { // si post es null, significa que no se encontró ningún post
-            return res.status(404).send({ message: 'Post no encontrado' });
+            return res.status(404).send({ message: 'Post not found' });
         }
         if (post.userId.toString() !== req.user._id.toString()) {
-            return res.status(403).send({ message: 'Este pedido no es tuyo' });
+            return res.status(403).send({ message: 'This is not your post' });
         }
         next();
     } catch (error) {
         console.error(error)
-        return res.status(500).send({ error, message: 'Ha habido un problema al comprobar la autoría del post' })
+        return res.status(500).send({ error, message: 'There was a problem verifying the authorship of the post' })
     }
 
 }
@@ -51,15 +51,15 @@ const isAuthorCom = async (req, res, next) => {
     try {
         const comment = await Comment.findById(req.params._id);
         if (!comment) { // si comment es null, significa que no se encontró ningún comment
-            return res.status(404).send({ message: 'Comment no encontrado' });
+            return res.status(404).send({ message: 'Comment not found' });
         }
         if (comment.userId.toString() !== req.user._id.toString()) {
-            return res.status(403).send({ message: 'Este pedido no es tuyo' });
+            return res.status(403).send({ message: 'This is not your comment' });
         }
         next();
     } catch (error) {
         console.error(error)
-        return res.status(500).send({ error, message: 'Ha habido un problema al comprobar la autoría del commentario' })
+        return res.status(500).send({ error, message: 'There was a problem verifying the authorship of the comment' })
     }
 
 }
