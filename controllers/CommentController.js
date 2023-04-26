949e5fb42094de20ddc25e96a84012f848a581d8
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-04-26 16:53:55 +0200

    Fixed endpoint delete comment for post too

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -10,7 +10,8 @@ const CommentController = {
     try {
       const comment = await Comment.create({
         body: req.body.body,
-        userId: req.user._id
+        userId: req.user._id,
+        postId: req.params._id
       });
 
       await Post.findByIdAndUpdate(req.params._id,
@@ -30,6 +31,31 @@ const CommentController = {
     } catch (error) {
       console.error(error);
       res.status(500).send({ message: `There was a problem creating the comment: ${error.message}` });
+    }
+  },
+
+  async update(req, res) {
+    try {
+      const comment = await Comment.findByIdAndUpdate(
+        req.params._id,
+        { ...req.body, userId: req.user._id },
+        { new: true }
+      );
+      res.status(201).send({ message: "Comment update successfully", comment });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: `There was a problem creating the comment: ${error.message}` });
+    }
+  },
+
+  async delete(req, res) {
+    try {
+      const comment = await Comment.findByIdAndDelete(req.params._id);
+      await Post.findByIdAndUpdate(comment.postId, { $pull: { commentIds: comment._id } });
+      res.send({ message: "Deleted comment", comment });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: `There was a problem deleting the comment: ${error.message}` });
     }
   },
 };
--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,9 @@ const { authentication } = require("../middlewares/authentication");
 
 router.post('/create/:_id',authentication ,CommentController.create)
 router.get('/getAll', CommentController.getAll)
+router.put('/update/:_id',authentication, CommentController.update)
+router.delete('/delete/:_id',authentication, CommentController.delete)
+
 
 
 
