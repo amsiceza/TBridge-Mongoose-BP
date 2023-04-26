2447e852f053ec04fa7b0df1c4051fdc32561898
GuileSoler87 <guillermosoler87@gmail.com>
2023-04-26 16:28:37 +0200

    like/unlike for comments added, like/unlike for posts fixed

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -21,6 +21,47 @@ const CommentController = {
       res.status(500).send({ message: `There was a problem creating the comment: ${error.message}` });
     }
   },
+
+  //To like a post, only one like per user 
+  async like(req, res) {
+    try {
+      const comment = await Comment.findById(req.params._id);
+
+      if (comment.likes.includes(req.user._id)) {
+        return res.status(400).send({ message: "You already liked this comment" });
+      }
+
+      comment.likes.push(req.user._id);
+      await comment.save();
+
+      res.send(comment);
+    } catch (error) {
+      console.error(error);
+
+      res.status(500).send({ message: "There was a problem with your like" });
+    }
+  },
+
+  // // Remove like from comment, only remove own like
+  async unlike(req, res) {
+    try {
+      const comment = await Comment.findById(req.params._id);
+
+      if (!comment.likes.includes(req.user._id)) {
+        return res.status(400).send({ message: "You haven't liked this comment yet" });
+      }
+
+      const index = comment.likes.indexOf(req.user._id);
+      comment.likes.splice(index, 1);
+      await comment.save();
+
+      res.send(comment);
+    } catch (error) {
+      console.error(error);
+
+      res.status(500).send({ message: "There was a problem with your unlike" });
+    }
+  }
 };
 
 module.exports = CommentController;
--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const CommentController = require('../controllers/CommentController');
 const { authentication } = require("../middlewares/authentication");
 
-router.post('/create/:_id',authentication ,CommentController.create)
+router.post('/create/:_id',authentication , CommentController.create)
+router.put('/likes/:_id', authentication, CommentController.like);
+router.put('/unlike/:_id', authentication, CommentController.unlike);
 
 
 
