a52012f0c56acc7c57dbe9a90b075ac1197a8d08
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-04-27 17:02:50 +0200

    For rebasing

--- routes/comments.js
+++ routes/comments.js
@@ -5,10 +5,13 @@ const { authentication, isAuthorCom } = require("../middlewares/authentication")
 
 router.post('/create/:_id',authentication ,CommentController.create)
 router.get('/getAll', CommentController.getAll)
+<<<<<<< HEAD
 router.put('/update/:_id',authentication, isAuthorCom, CommentController.update)
 router.delete('/delete/:_id',authentication, isAuthorCom, CommentController.delete)
 router.put('/likes/:_id', authentication, CommentController.like);
 router.put('/unlike/:_id', authentication, CommentController.unlike);
+=======
+>>>>>>> bdd4441 (Endpoint get all comments)
 
 
 
