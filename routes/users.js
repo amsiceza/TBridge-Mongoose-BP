50ad0425495afdc762fa8aac904785e1983e4246
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-04-27 17:06:02 +0200

    Rebasing

--- routes/users.js
+++ routes/users.js
@@ -9,10 +9,13 @@ router.post('/login',UserController.login)
 router.delete('/logout',authentication , UserController.logout)
 router.get('/getUser', authentication, UserController.getUser)
 router.get('/confirm/:emailToken', UserController.confirm)
+<<<<<<< HEAD
 router.get("/getById/:_id", authentication, UserController.getById);
 router.get("/getByUsername/:username", authentication, UserController.getByUsername);
 router.put('/follow/:_id', authentication, UserController.follow);
 router.put('/unfollow/:_id', authentication, UserController.unfollow);
+=======
+>>>>>>> 5ed82da (nodemailer fixed)
 
 
 module.exports = router;
