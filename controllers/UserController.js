a8ab5f6069832f074192e85a487c8109537c0afe
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-04-27 16:38:05 +0200

    Updated endpoint unfollow

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -163,7 +163,7 @@ const UserController = {
           { new: true }
         );
         
-        res.status(200).send({message: `Now you are following ${user.username}`, user});
+        res.send(user);
       } catch (error) {
         console.error(error);
   
@@ -189,7 +189,7 @@ const UserController = {
           { new: true }
         );
         
-        res.send(user);
+        res.status(200).send({message: `Now you are not following ${user.username}`, user});
       } catch (error) {
         console.error(error);
   
