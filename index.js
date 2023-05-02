87523d7f2556d71f9d023465ea1590c9e3dfd162
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-05-02 13:25:27 +0200

    Add cors

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const { dbConnection } = require("./config/config")
 const { handleTypeError }= require('./middlewares/errors.js');
+const cors = require("cors")
 const swaggerUI = require('swagger-ui-express')
 const docs = require('./docs/index')
 
 
 app.use(express.json())
+app.use(cors())
 
 dbConnection()
 
